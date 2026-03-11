7baed46647f7174008d27768f1700f2df2827b66
u2120220582 via LDAP <chenyuming@mail.nankai.edu.cn>
2026-03-11 20:20:52 +0800

    debug

--- mmrotate/models/backbones/__init__.py
+++ mmrotate/models/backbones/__init__.py
@@ -3,4 +3,7 @@ from .re_resnet import ReResNet
 from .stripnet import StripNet
 from .pkinet import PKINet
 from .stripnet_axial_mamba import StripMambaNet
-__all__ = ['ReResNet','StripNet','PKINet','StripMambaNet']
+from .stripnet__uni_cross_mamba import StripSMambaNet
+from .stripnet_bi_cross_mamba import StripDMambaNet
+
+__all__ = ['ReResNet', 'StripNet', 'PKINet', 'StripMambaNet', 'StripSMambaNet', 'StripDMambaNet']
